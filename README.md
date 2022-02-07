b6ac44de22e8e8729a644831ffb8425a44cc06e7
yairmc666 <yairmc666@hotmail.com>
2022-02-06 19:12:23 -0700

    prefinalizacion de la pagina de festival de musica

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# Finalizando la seccion de video
-# creando mixin para grid
+# Minificando peso de las imagenes con gulp-imagemin, gulp-avif, gulp-webp y gulp-cache
+# Creando funciones para convertir y direccionar a una carpeta dichas imagenes
+# Utilizando y crando snipets para html sobre creacion de imagenes
+# creacion y finalizacion del lineup y calendario de la pagina
+# se agrego una ventana modal con previsualizacion de imagenes
 
-
-# Udemy video 182
+# Udemy video 194
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
-const {src, dest, watch}=require('gulp');
+const {src, dest, watch, parallel}=require('gulp');
+
+// CSS
 const sass= require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
 
+// Imagenes
+const imagemin = require('gulp-imagemin')
+const webp = require('gulp-webp');
+const cache= require('gulp-cache');
+const avif = require('gulp-avif');
 
 function css(done){
     src('src/scss/**/*.scss')//identificar el archivo  .SCSS a compilar
@@ -12,10 +19,62 @@ function css(done){
     done();
 }
 
+function imagenes(done){
+    const opciones={
+        optimizationLevel:3
+    }
+
+    src('src/img/**/*.{png,jpg}')
+    .pipe(cache(imagemin(opciones)))
+    .pipe(dest('build/img'))
+
+    done();
+}
+
+function versionWebp(done){
+
+    const opciones={
+        quality:50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+    .pipe(webp(opciones) )
+    .pipe(dest('build/img') )
+
+
+    done();
+}
+
+function versionAvif(done){
+
+    const opciones={
+        quality:50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+    .pipe(avif(opciones) )
+    .pipe(dest('build/img') )
+
+
+    done();
+}
+
+function javascript(done){
+    src('src/js/**/*.js')
+    .pipe(dest('build/js'));
+
+    done();
+}
+
 function dev(done){
     watch('src/scss/**/*.scss', css);
+    watch('src/js/**/*.js', javascript)
     done();
 }
 
 exports.css=css;
-exports.dev=dev;
+exports.js=javascript;
+exports.imagenes=imagenes;
+exports.versionWebp=versionWebp;
+exports.versionAvif=versionAvif;
+exports.dev= parallel(imagenes,versionWebp, versionAvif,javascript, dev); 
